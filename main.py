35a2d33228ccdc2be21e8e247688ccd467e460b9
Windsao <swu23@gwu.edu>
2022-02-16 07:09:27 -0600

    fix distillation

--- main.py
+++ main.py
@@ -19,12 +19,15 @@ def main():
         t.test()
     else:
         if checkpoint.ok:
+            temp = args.model
             if args.distil:
+                args.model = 'EDSR'
                 _teacher = model.Model(args, checkpoint)
                 #_teacher.myload('../experiment/5_1_3/model/model_best.pt') 
                 _teacher.myload('../experiment/baseline_edsr_x2_100_vaild/model/model_best.pt') 
             else:
                 _teacher = None
+            args.model = temp
             loader = data.Data(args)
             _model = model.Model(args, checkpoint)
             _loss = loss.Loss(args, checkpoint) if not args.test_only else None
