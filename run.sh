fce86d9917dfb1a297c1985c56db0f33237910b4
Windsao <swu23@gwu.edu>
2022-02-17 05:07:07 -0600

    fix

--- run.sh
+++ run.sh
@@ -4,10 +4,10 @@
 #HIP_VISIBLE_DEVICES=$1 python main.py --model EDSR --scale 2 --patch_size 96 --save debug --reset
 #HIP_VISIBLE_DEVICES=$1 python main.py --model EDSR --scale 2 --patch_size 96 --beta $2 --save distil_base_$2 --reset --distil > test_$1.txt 2>&1 &
 
-HIP_VISIBLE_DEVICES=$1 python main.py --model RFDN --scale 2 --patch_size 64 --save debug --reset --distil --cutmix
+#HIP_VISIBLE_DEVICES=$1 python main.py --model RFDN --scale 2 --patch_size 64 --save debug --reset --distil --cutmix
 #HIP_VISIBLE_DEVICES=$1 python main.py --model RFDN --scale 2 --patch_size 64 --save baseline_rfdn_x2 --reset > test_$1.txt 2>&1 &
 #HIP_VISIBLE_DEVICES=$1 python main.py --model RFDN --scale 2 --patch_size 64 --beta $2 --save distil_rfdn_x2_$2 --reset --distil > test_$1.txt 2>&1 &
-#HIP_VISIBLE_DEVICES=$1 python main.py --model RFDN --scale 2 --patch_size 64 --beta $2 --save cutmix_rfdn_x2_$2 --reset --distil --cutmix > test_$1.txt 2>&1 &
+HIP_VISIBLE_DEVICES=$1 python main.py --model RFDN --scale 2 --patch_size 64 --beta $2 --save cutmix0.05_rfdn_x2_$2 --reset --distil --cutmix --aug_alpha 0.05 > test_$1.txt 2>&1 &
 
 #HIP_VISIBLE_DEVICES=$1,$2 python main.py --model EDSR --scale 2 --save adv_edsr_x2 --n_resblocks 32 --n_feats 256 --res_scale 0.1 --reset > adv_$1.txt 2>&1 &
 #HIP_VISIBLE_DEVICES=0,1,2,3,4 python main.py --model EDSR --scale 2 --save test --n_resblocks 32 --n_feats 256 --res_scale 0.1 --reset
